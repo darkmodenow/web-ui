06a05088ea0e39cb91a2d4b2b1cf5c4b9a054210
lanyado <lanyado98@gmail.com>
2025-03-24 22:39:26 -0400

    Firebase Hosting

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,8 @@ import { initializeApp } from 'firebase/app';
 
 const queryClient = new QueryClient();
 
+console.log("🔥 VITE_APIKEY during build:", process.env.VITE_APIKEY);
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_APIKEY,
   authDomain: import.meta.env.VITE_AUTHDOMAIN,
